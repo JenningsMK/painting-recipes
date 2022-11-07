0f35bbde03d2a0a204a93ac190e9c8bff42d25f2
Mark <magzone@hotmail.co.uk>
2022-11-07 12:25:45 +0000

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- skip links needed -->
+    <main>
+      <h1>Painting Guides</h1>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
