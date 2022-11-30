bc7228318414e9e7e2ca2718d8b0cec34eeb61b4
Mark <magzone@hotmail.co.uk>
2022-11-30 19:37:32 +0000

    PR-7 - Configuring Tailwind
    Have set up Tailwind with colours and fonts, have also started to create a main css to put global settings and a fonts css file.

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/**
+ * @type {import('tailwindcss').Config}
+ * @see https://tailwindcss.com/docs/configuration
+ */
+module.exports = {
+  content: ['./pages/*.vue'],
+  theme: {
+    colors: {
+      light: {
+        primary: 'hsla(281 64% 31% / 1)',
+        text: 'hsla(210 40% 98% / 1)',
+      },
+      dark: {
+        primary: 'hsla(281 75% 14% / 1)',
+        text: 'hsla(217 33% 17% / 1)',
+        background: 'hsla(270 72% 7% / 1)',
+      },
+      overlay: 'rgba(126 127 130 / 0.4)',
+    },
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', ...defaultTheme.fontFamily.serif],
+        sig: ['Signika Negative', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+}
