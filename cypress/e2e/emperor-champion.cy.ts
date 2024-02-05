8d6049c43860d93e93435561b268421f25952dbf
Mark <magzone@hotmail.co.uk>
2024-02-05 17:10:53 +0000

    Update navigation and style behaviour across the site
    This commit involves a series of changes affecting navigation, style behaviour, and content updates site-wide. The changes introduced enable smooth navigation functionalities, introduce new animations, and correct misspellings for better user experience. The code includes adjustments in the navigation routing and UI interactive elements, as well as renaming of CSS classes, enhanced accessibility, and end-to-end Cypress tests.

--- cypress/e2e/emperor-champion.cy.ts
+++ cypress/e2e/emperor-champion.cy.ts
@@ -22,7 +22,19 @@ describe('That the navigation is working as expected', () => {
 
   it('Has the slide in & out motion when the button is clicked', () => {
     cy.get('nav.navigation').should('not.be.visible');
+    cy.get('nav.navigation').should('have.attr', 'aria-hidden');
+    cy.get('nav.navigation').should('have.attr', 'inert');
     cy.contains('#menu-button', 'open menu').trigger('click');
     cy.get('nav.navigation').should('be.visible');
+    cy.get('nav.navigation').should('not.have.attr', 'aria-hidden');
+    cy.get('nav.navigation').should('not.have.attr', 'inert');
+  });
+
+  it('Adds the aria-hidden and inert attributes to the main when the overlay is open', () => {
+    cy.get('main').should('not.have.attr', 'aria-hidden');
+    cy.get('main').should('not.have.attr', 'inert');
+    cy.contains('#menu-button', 'open menu').trigger('click');
+    cy.get('main').should('have.attr', 'aria-hidden');
+    cy.get('main').should('have.attr', 'inert');
   });
 });
